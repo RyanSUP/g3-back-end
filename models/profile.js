1d40965868d8b95c6e74ce2271cf9a667c8a70b9
Abygaelle <abygaelleb02@gmail.com>
2022-03-27 13:32:36 -0400

    add avatar into profile

--- models/profile.js
+++ models/profile.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema
 
 const profileSchema = new mongoose.Schema({
   email: {type: String, required: true, lowercase: true, unique: true},
+  avatar: String,
   name: String,
   games: [{ type: Schema.Types.ObjectId, ref: "Game" }],
 },{
