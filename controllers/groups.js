0c5e9ef0577449884b9c33546a47bc4f02edcc19
RMo <morici23@gmail.com>
2022-03-29 09:56:41 -0400

    Correction - delete gatherings

--- controllers/groups.js
+++ controllers/groups.js
@@ -1,5 +1,4 @@
 import { Group } from "../models/group.js";
-
 //we need to also set the manager of the group to the user who is logged in
 //then push the profile of the logged in user to the group
 //AND push the group into the profile of the logged in user
