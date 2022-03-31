d6511e3feacd3c82522c1485e625fa684c2ecada
RMo <morici23@gmail.com>
2022-03-30 20:45:13 -0400

    Update model and controller to take in new game data

--- models/game.js
+++ models/game.js
@@ -8,6 +8,14 @@ const gameSchema = new Schema({
   description_preview: {type: String},
   thumb_url: {type: String},
   image_url: {type: String},
+  min_players: {type: Number},
+  max_players: {type: Number},
+  min_playtime: {type: Number},
+  max_playtime: {type: Number},
+  min_age: {type: Number},
+  players: {type: String},
+  playtime: {type: String},
+
 }, {
   timestamps: true
 })
